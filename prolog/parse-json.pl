6e392c0b41ad4ea73611c586e2b20e09e5193302
andreagosti9 <andrea.agosti9@gmail.com>
2023-03-29 15:56:42 +0200

    found the need to change arc facts into rules

--- prolog/parse-json.pl
+++ prolog/parse-json.pl
@@ -1,29 +1,51 @@
 %%%% -*- Mode: Prolog -*-
 
+initial(i).
+final(f).
+
+% Descrizione logica dei fatti 'arc'
+% arc(nodo, carattere in input, stack top attuale, nodo di arrivo, nuovo stack top)
+arc(i, "{", "", a, "{").
+arc(a, "[", "{", b, "[").
+arc(a, "{", "{", a, "{").
+arc(a, "}", "{", c, ""). % Caso: pop()
 
 
-% @CN (current node) 
-    % nodo di inizio dell'arco
-% @EN (end node) 
-    % nodo di arrivo secondo l'arco
-% @X () 
-    % nuovo elemento da stackare
-% @NS (new stack) 
-    % stack con X stackato
-% @[I (input) | IR (input rest)] 
-    % I input corrente, IR il resto degli input
-% @[CST (current stack top) | SR (stack rest)] 
-    % CST cima dello stack, SR il resto dello stack 
-
-accept(CN, [], _) :- final(CN).
-accept(CN, [I | IR], [CST | SR]) :-
-    arc(CN, I, CST, EN, X), !,
-    append([X], [CST | SR], NS),
-    accept(EN, IR, NS).
-
-    (length(IR, 0) ->
-	 final(I) ; accept(EN, IR, NS)).
+arc(i, "[", "", b, "[").
+arc(b, "{", "[", a, "{").
+arc(b, "[", "[", b, "[").
+arc(b, "]", "[", c, ""). % Caso: pop()
 
 
-brackets_match(""). 
-brackets_match(Y) :- atom_chars(Y, L), accept(p, L, [_]).
+arc(c, "", "{", a, ""). % Caso: lascio invariato lo stack
+arc(c, "", "[", b, ""). % Caso: lascio invariato lo stack
+arc(c, "", "", f, ""). % Caso: lo stack è vuoto e lo lascio tale
+
+
+
+% CASO INIZIALE NULLO
+% Se l'input risulta vuoto, lo stack anche è come se fossi sul nodo iniziale, quindi va tutto bene.
+accept([], []).
+
+% CASO INIZIALE
+% "Overloading" di convenienza per la regola 'accept' in modo da nascondere più dettagli possibile all'esterno di questa zona.
+accept(LIST, []) :- 
+    initial(NODE), !, 
+    accept(NODE, LIST, [_]).
+
+% CASO FINALE
+% Se l'input risulta vuoto e lo stack anche, sono arrivato alla fine con successo IFF il noto su cui mi trovo è quello finale.
+accept(NODE, [], []) :- final(NODE).
+
+
+% Regola di accettazione generica 
+% TODO: bisogna considerare il pop dallo stack
+accept(NODE, [I | INPUT_REST], [TOP | STACK]) :-
+    arc(NODE, I, TOP, END_NODE, X), !,
+    append([X], [TOP | STACK], NEW_STACK),
+    accept(END_NODE, INPUT_REST, NEW_STACK).
+
+
+
+% ===== MAIN ===== %
+brackets_match(STRING) :- atom_chars(STRING, INPUT), accept(INPUT, []).
