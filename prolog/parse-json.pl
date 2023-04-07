5780a64b95ac0ca9d15d091330700069c90723a4
andreagosti9 <andrea.agosti9@gmail.com>
2023-04-08 00:33:01 +0200

    trying to correct bug

--- prolog/parse-json.pl
+++ prolog/parse-json.pl
@@ -5,39 +5,31 @@ pop([_ | TAIL], UPDATED_S) :- UPDATED_S = TAIL, !.
 
 initial(i).
 final(f).
-final(c).
 
 
 % Descrizione logica dei fatti 'arc'.
+%  ==> arc(nodo, carattere in input, stack attuale, nodo di arrivo, stack aggiornato)
 % La regola append() aggiunge in testa; controintuitivamente la testa (top) dello stack sta a sinistra.
 % Forse è necessario fare controlli sullo stack attuale per vedere cosa ci sia in testa (per capire se effettivamente mi trovo sul nodo dell'automa corretto), ma tecnicamente se le definizioni stesse degli archi sono giuste non dovrebbe servire, perchè verrei già indirzzato correttamente.
-
-% arc(nodo, carattere in input, stack attuale, nodo di arrivo, stack aggornato)
 
 
 % Parentesi GRAFFE
 arc(i, '{', CURR, a, UPDATED) :- push('{', CURR, UPDATED).
-arc(a, '[', CURR, b, UPDATED) :- push('[', CURR, UPDATED).
 arc(a, '{', CURR, a, UPDATED) :- push('{', CURR, UPDATED).
-
-% Caso: pop()
 arc(a, '}', CURR, c, UPDATED) :- pop(CURR, UPDATED).
+arc(a, '}', ['{'], c, _).
+% arc(c, _, [H | _], a, _) :- H == '{'.
+% arc(a, [], [], c, []).
+arc(c, [], [], f, []).
 
 
 % Parentesi QUADRE
-arc(i, '[', CURR, b, UPDATED) :- push('[', CURR, UPDATED).
-arc(b, '{', CURR, a, UPDATED) :- push('{', CURR, UPDATED).
-arc(b, '[', CURR, b, UPDATED) :- push('[', CURR, UPDATED).
-
-% Caso: pop()
-arc(b, ']', CURR, c, UPDATED) :- pop(CURR, UPDATED). 
-
-
-% Se qui volessi fare controlli (di cui al commento sopra) al posto del primo _ dovrei usare una variabile, come sopra, ad esempio CURR.
-arc(c, '{', _, a, _). 
-arc(c, '[', _, b, _). 
-arc(c, '', [], f, []).
-
+% arc(i, '[', CURR, b, UPDATED) :- push('[', CURR, UPDATED).
+% arc(b, '{', CURR, a, UPDATED) :- push('{', CURR, UPDATED).
+% arc(b, '[', CURR, b, UPDATED) :- push('[', CURR, UPDATED).
+% arc(b, ']', CURR, c, UPDATED) :- pop(CURR, UPDATED).
+% arc(b, [], [], c, []).
+% arc(c, _, [HEAD | _], b, _) :- HEAD == '['.
 
 
 % Le regole sono ordinate in senso logico; la funzione principale chiama accept/2, se nullo esegue il fatto qui sotto. Se non nullo esegue il caso accept/2 non generico. Se va tutto bene questo chiama accept/3 e alla fine sempre se va tutto bene chiama accept/3 caso finale.
@@ -56,10 +48,6 @@ accept(LIST, []) :-
 accept(NODE, [I | INPUT_REST], STACK) :-
     arc(NODE, I, STACK, END_NODE, UPDATED_STACK), !,
     accept(END_NODE, INPUT_REST, UPDATED_STACK).
-
-% CASO FINALE
-% Se l'input risulta vuoto e lo stack anche, sono arrivato alla fine con successo se solo se il nodo su cui mi trovo è quello finale (se nulla è andato storto dovrebbe essere proprio così).
-accept(NODE, [], []) :- final(NODE).
 
 % ===== MAIN ===== %
 brackets_match(STRING) :- atom_chars(STRING, INPUT), accept(INPUT, []).
