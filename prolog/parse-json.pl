0ded0b67816845ca380a6cf678dbb62f057d28b5
andreagosti9 <andrea.agosti9@gmail.com>
2023-04-10 12:10:34 +0200

    added extra arg to save parsed JSON

--- prolog/parse-json.pl
+++ prolog/parse-json.pl
@@ -10,35 +10,36 @@ final(f).
 
 % Descrizione logica dei fatti 'arc'.
 %  ==> arc(nodo, carattere in input, stack attuale, nodo di arrivo, stack aggiornato)
+% BS = brackets stack
+% UBS = updated brackets stack
 % La regola append() aggiunge in testa; controintuitivamente la testa (top) dello stack sta a sinistra.
 % Forse è necessario fare controlli sullo stack attuale per vedere cosa ci sia in testa (per capire se effettivamente mi trovo sul nodo dell'automa corretto), ma tecnicamente se le definizioni stesse degli archi sono giuste non dovrebbe servire, perchè verrei già indirzzato correttamente.
 
 
 % Accettazione dei caratteri liberi.
 % Non ho bisogno di "poppare" perchè quando 'accept' chiama 'arc' uso la notazione delle liste [Testa | Coda].
-arc(N, ' ', CURR, N, CURR).
-arc(N, '\n', CURR, N, CURR).
-arc(N, '\t', CURR, N, CURR).
-arc(N, '\r', CURR, N, CURR).
+arc(N, ' ', BS, N, BS, JSON).
+arc(N, '\n', BS, N, BS, JSON).
+arc(N, '\t', BS, N, BS, JSON).
+arc(N, '\r', BS, N, BS, JSON).
 
 % Parentesi GRAFFE
-arc(i, '{', CURR, a, UPDATED) :- push('{', CURR, UPDATED).
-arc(a, '{', CURR, a, UPDATED) :- push('{', CURR, UPDATED).
-arc(a, '}', CURR, a, UPDATED) :- pop(CURR, UPDATED).
-arc(a, [], [], f, []).
+arc(i, '{', BS, c, UBS, JSON) :- push('{', BS, UBS).
+arc(c, '{', BS, c, UBS, JSON) :- push('{', BS, UBS).
+arc(c, '}', BS, c, UBS, JSON) :- pop(BS, UBS).
+arc(c, [], [], f, [], JSON).
 
 % Parentesi QUADRE
-arc(i, '[', CURR, b, UPDATED) :- push('[', CURR, UPDATED).
-arc(b, '[', CURR, b, UPDATED) :- push('[', CURR, UPDATED).
-arc(b, ']', CURR, b, UPDATED) :- pop(CURR, UPDATED).
-arc(b, [], [], f, []).
+arc(i, '[', BS, s, UBS, JSON) :- push('[', BS, UBS).
+arc(s, '[', BS, s, UBS, JSON) :- push('[', BS, UBS).
+arc(s, ']', BS, s, UBS, JSON) :- pop(BS, UBS).
+arc(s, [], [], f, []).
 
 % Archi di passaggio
-arc(a, '[', CURR, b, UPDATED) :- push('[', CURR, UPDATED).
-arc(b, '{', CURR, a, UPDATED) :- push('{', CURR, UPDATED).
-arc(b, '}', ['{' | TAIL], a, TAIL).
-arc(a, ']', ['[' | TAIL], b, TAIL).
-
+arc(c, '[', BS, s, UBS, JSON) :- push('[', BS, UBS).
+arc(s, '{', BS, c, UBS, JSON) :- push('{', BS, UBS).
+arc(s, '}', ['{' | TAIL], c, TAIL, JSON).
+arc(c, ']', ['[' | TAIL], s, TAIL, JSON).
 
 
 % Le regole sono ordinate in senso logico; la funzione principale chiama accept/2, se nullo esegue il fatto qui sotto. Se non nullo esegue il caso accept/2 non generico. Se va tutto bene questo chiama accept/3 e alla fine sempre se va tutto bene chiama accept/3 caso finale.
