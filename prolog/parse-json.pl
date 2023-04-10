96ab21fd6ec5a1c14785467b020ad08405f48b99
andreagosti9 <andrea.agosti9@gmail.com>
2023-04-10 10:45:22 +0200

    brackets matcher works

--- prolog/parse-json.pl
+++ prolog/parse-json.pl
@@ -16,21 +16,20 @@ final(f).
 % Parentesi GRAFFE
 arc(i, '{', CURR, a, UPDATED) :- push('{', CURR, UPDATED).
 arc(a, '{', CURR, a, UPDATED) :- push('{', CURR, UPDATED).
-arc(a, '}', CURR, c, UPDATED) :- pop(CURR, UPDATED).
-arc(a, '}', ['{'], c, _).
-% arc(c, _, [H | _], a, _) :- H == '{'.
-% arc(a, [], [], c, []).
-arc(c, [], [], f, []).
-
+arc(a, '}', CURR, a, UPDATED) :- pop(CURR, UPDATED).
+arc(a, [], [], f, []).
 
 % Parentesi QUADRE
-% arc(i, '[', CURR, b, UPDATED) :- push('[', CURR, UPDATED).
-% arc(b, '{', CURR, a, UPDATED) :- push('{', CURR, UPDATED).
-% arc(b, '[', CURR, b, UPDATED) :- push('[', CURR, UPDATED).
-% arc(b, ']', CURR, c, UPDATED) :- pop(CURR, UPDATED).
-% arc(b, [], [], c, []).
-% arc(c, _, [HEAD | _], b, _) :- HEAD == '['.
+arc(i, '[', CURR, b, UPDATED) :- push('[', CURR, UPDATED).
+arc(b, '[', CURR, b, UPDATED) :- push('[', CURR, UPDATED).
+arc(b, ']', CURR, b, UPDATED) :- pop(CURR, UPDATED).
+arc(b, [], [], f, []).
 
+% Archi di passaggio
+arc(a, '[', CURR, b, UPDATED) :- push('[', CURR, UPDATED).
+arc(b, '{', CURR, a, UPDATED) :- push('{', CURR, UPDATED).
+arc(b, '}', ['{' | TAIL], a, TAIL).
+arc(a, ']', ['[' | TAIL], b, TAIL).
 
 % Le regole sono ordinate in senso logico; la funzione principale chiama accept/2, se nullo esegue il fatto qui sotto. Se non nullo esegue il caso accept/2 non generico. Se va tutto bene questo chiama accept/3 e alla fine sempre se va tutto bene chiama accept/3 caso finale.
 
@@ -38,16 +37,25 @@ arc(c, [], [], f, []).
 % Se l'input risulta vuoto, lo stack anche è come se fossi sul nodo iniziale, quindi va tutto bene.
 accept([], []).
 
+
 % CASO INIZIALE GENERICO
 % "Overloading" di convenienza per la regola 'accept' in modo da nascondere più dettagli possibile all'esterno di questa zona.
 accept(LIST, []) :- 
     initial(NODE), !, 
     accept(NODE, LIST, []).
 
+
+% CASO DI CONTROLLO FINALE
+accept(NODE, [], []) :- 
+    arc(NODE, [], [], END_NODE, []), !, 
+    final(END_NODE).
+
+
 % Regola di accettazione generica.
 accept(NODE, [I | INPUT_REST], STACK) :-
     arc(NODE, I, STACK, END_NODE, UPDATED_STACK), !,
     accept(END_NODE, INPUT_REST, UPDATED_STACK).
+
 
 % ===== MAIN ===== %
 brackets_match(STRING) :- atom_chars(STRING, INPUT), accept(INPUT, []).
