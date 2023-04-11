e3c6c40a1e722143bbdf6bba0491dadbaa4bf23c
andreagosti9 <andrea.agosti9@gmail.com>
2023-04-11 21:31:30 +0200

    added free chars

--- prolog/parse-json.pl
+++ prolog/parse-json.pl
@@ -1,4 +1,7 @@
 %%%% -*- Mode: Prolog -*-
+
+
+% ========== ========== ========== ========== ========== %
 % Regola generale importante! Se uso _ per una variabile poi ho difficoltà a stamparne il valore, perchè le variabili anonime vengono stampate con il come  _09090. Meglio usare una variabile normale e fare in modo che non sia singleton. Inoltre la variabile normale deve essere comunqua inizializzata o produrrà un effetto simile.
 
 
@@ -9,21 +12,30 @@ initial(i).
 final(f).
 
 
+% ========== ========== ========== ========== ========== %
 % Descrizione logica dei fatti 'arc'.
-%  ==> arc(nodo, carattere in input, stack attuale, nodo di arrivo, stack aggiornato)
+% (nodo, carattere in input, stack attuale, nodo di arrivo, stack aggiornato, json, json aggiornato)
 % BS = brackets stack
 % UBS = updated brackets stack
 % La regola append() aggiunge in testa; controintuitivamente la testa (top) dello stack sta a sinistra.
-% Forse è necessario fare controlli sullo stack attuale per vedere cosa ci sia in testa (per capire se effettivamente mi trovo sul nodo dell'automa corretto), ma tecnicamente se le definizioni stesse degli archi sono giuste non dovrebbe servire, perchè verrei già indirzzato correttamente.
 
-/* DA AGGIORNARE
+
 % Accettazione dei caratteri liberi.
 % Non ho bisogno di "poppare" perchè quando 'accept' chiama 'arc' uso la notazione delle liste [Testa | Coda].
-arc(N, ' ', BS, N, BS).
-arc(N, '\n', BS, N, BS).
-arc(N, '\t', BS, N, BS).
-arc(N, '\r', BS, N, BS).
+arc(N, ' ', BS, N, BS, S_, S_).
+arc(N, '\n', BS, N, BS, S_, S_).
+arc(N, '\t', BS, N, BS, S_, S_).
+arc(N, '\r', BS, N, BS, S_, S_).
 
+
+% Parentesi QUADRE
+arc(i, '[', BS, s, UBS, S_, S_) :- push('[', BS, UBS).
+arc(s, '[', BS, s, UBS, J, UJ) :- push('[', BS, UBS), push(['#'], J, UJ).
+arc(s, ']', BS, s, UBS, S_, S_) :- pop(BS, UBS).
+arc(s, [], [], f, [], S_, S_).
+
+
+/* DA AGGIORNARE
 % Parentesi GRAFFE
 arc(i, '{', BS, c, UBS) :- push('{', BS, UBS).
 arc(c, '{', BS, c, UBS) :- push('{', BS, UBS).
@@ -37,14 +49,7 @@ arc(s, '}', ['{' | TAIL], c, TAIL).
 arc(c, ']', ['[' | TAIL], s, TAIL).
 */
 
-% Parentesi QUADRE
-arc(i, '[', BS, s, UBS, S_, S_) :- push('[', BS, UBS).
-arc(s, '[', BS, s, UBS, J, UJ) :- push('[', BS, UBS), push('ciao', J, UJ).
-arc(s, ']', BS, s, UBS, S_, S_) :- pop(BS, UBS).
-arc(s, [], [], f, [], S_, S_).
-
-
-
+% ========== ========== ========== ========== ========== %
 % Le regole sono ordinate nel possibile senso di chiamata.
 
 % CASO INIZIALE NULLO
