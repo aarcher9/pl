493424b668bbf0d279fb0b75b7e99e913ffcefa3
andreagosti9 <andrea.agosti9@gmail.com>
2023-06-04 14:47:53 +0200

    removed old code

--- esame0623/Agosti_Andrea_826296_mvpoly_LP_202306/prolog/mvpoly.pl
+++ esame0623/Agosti_Andrea_826296_mvpoly_LP_202306/prolog/mvpoly.pl
@@ -22,68 +22,11 @@ clean_push(Item, List, Out) :- push(Item, List, Out).
 % == == %
 
 
-/*
-
-% == [ Divisione in stringhe di monomi ] == %
-% Divide la stringa in input in monomi. Se splitto lasciando '' nel 3 parametro i segni '-' doppi non vengono considerati come uno solo. Questo dovrebbe fare in modo che il programma torni false se il polinomio inserito non è correttamente scritto.
-split_polynomial(Expression, MonomialsList) :-
-        split_string(Expression, '-', '-', [H | T]),
-        reinsert_minus(T, Tail),
-        push(H, Tail, PolynomialList),
-        strip_pluses(PolynomialList, Nested),
-        flatten(Nested, Dirty),
-        remove_empty_items(Dirty, MonomialsList).
-
-
-% Fa in modo che la lista venga pulita da eventuali '' avanzati.
-remove_empty_items([], []).
-remove_empty_items([H | T], Out) :-
-        remove_empty_items(T, Tail),
-        clean_push(H, Tail, Out).
-
-
-% Splitta l'espressione basandosi sui segni '-'.
-clean_minuses([], []).
-clean_minuses([H | T], Out) :- 
-        split_string(H, '-', '-', A),
-        clean_minuses(T, B),
-        push(A, B, Out).
-
-strip_minuses(Expression, Out) :- 
-        split_string(Expression, '-', '-', O),
-        clean_minuses(O, Out).
-
-
-% Rimuove i segni '+' dalla lista di polinomi una lista di monomi.
-strip_pluses([], []).
-strip_pluses([H | T], Out) :- 
-        split_string(H, '+', '+', A),
-        strip_pluses(T, B),
-        push(A, B, Out).
-
-% strip_pluses([], []).
-% strip_pluses([H | T], Out) :- clean_pluses([H | T], Out).
-
-
-% Accorpa i '-' rimossi dallo split dove andrebbero messi. I '+' sono neutri (1 o 100 non cambia il senso del monomio). Fa in modo che se nel polinomio passato a split_polynomial ci sono catene di '-' vengano eliminati tutti meno uno, quello del monomio.
-reinsert_minus([], []).
-
-reinsert_minus([H | T], Out) :-
-        reinsert_minus(T, Tail),
-        atom_concat('-', H, Head),
-        push(Head, Tail, Out).
-
-% == == %
-
-*/
 
 % == [ Parsing monomi ] == %
 % Si suppone che arrivi un carattere alla volta.
 is_digit(Char) :- atom_codes(Char, [H | _]), H >= 48, H =< 57.
 is_var_symbol(Char) :- atom_codes(Char, [H | _]), H >= 97, H =< 122.
-
-% Per uno stack l'append fa eseguito 'al contrario' in modo che usando la notazione [Head | Tail] lo stack top sia sempre la Head.
-% stack_push(Item, Stack, New) :- push(Stack, [Item], New).
 
 
 % Funzionde delta del PDA che riconosce un monomio.
@@ -135,10 +78,11 @@ pda(State, [], [H | T], [H | T]) :-
         final(State),
         (is_var_symbol(H) ; is_digit(H)).
 
-
+% Predicato high-level per il parsing dei monomi.
 as_monomial(Expression, Monomial) :-
         atom_chars(Expression, L),
-        pda('a', L, [], Monomial).
+        pda('a', L, [], Specular),
+        reverse(Specular, Monomial).
 
 
 % == == %
