c32cea5c36aaeddad488cc6ea3663e14df100c5d
andreagosti9 <andrea.agosti9@gmail.com>
2023-06-30 17:30:38 +0200

    added matrix code

--- c/matrix.c
+++ c/matrix.c
@@ -5,21 +5,30 @@ typedef int rows;
 typedef int columns;
 typedef double* items;
 
+// Cosí facendo non devo tipizzare con 'struct Matrix' ma solo 'Matrix'
 typedef struct {
         rows n_of_rows;
         columns n_of_columns;
         items elements;
 } Matrix;
 
-// Matrix new_matrix(int n_of_rows, int n_of_columns) {
-//         Matrix m;
+Matrix* new_matrix(int n_of_rows, int n_of_columns) {
 
-//         m.n_of_rows = n_of_rows;
-//         m.n_of_columns = n_of_columns;
-//         m.elements = (double *) malloc((n_of_rows *n_of_columns) * sizeof(int));
-// };
+        // Prolisso ma sensato
+        Matrix* m = malloc(sizeof(Matrix));
+
+        (*m).n_of_rows = n_of_rows;
+        (*m).n_of_columns = n_of_columns;
+        (*m).elements = malloc((n_of_rows * n_of_columns) * sizeof(int));
+
+        return m;
+};
 
 int main() {
+
+        Matrix* m = new_matrix(4, 5);
+
+        printf("%i, %i", (*m).n_of_rows, (*m).n_of_columns);
 
         return 0;
 }
