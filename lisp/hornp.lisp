baa4fccffed5da1790c798656131c91f8968493d
andreagosti9 <andrea.agosti9@gmail.com>
2023-07-01 16:30:38 +0200

    added correct code hornp LISP

--- lisp/hornp.lisp
+++ lisp/hornp.lisp
@@ -1,15 +1,30 @@
-;; (defun check-disj (disj) 
-;;         (not (and 
-;;                 (not (eq `not (first (first disj))))
-;;                 (check-disj (rest disj)))))
+(defun suml (ls) 
+        (cond   ((not (null ls))
+                        (+ (first ls) (suml (rest ls))))
+                ((null ls)
+                        0)))
 
-(defun check-disj (disj) 
-        (print disj))
+(defun conj-assign-values (conj) 
+        (cond   ((null conj) 
+                        nil)
+                ((atom (first conj)) 
+                        (cons 1 (conj-assign-values (rest conj))))
+                ((not (eq `not (first (first conj))))
+                        (cons 1 (conj-assign-values (rest conj)))) 
+                ((eq `not (first (first conj)))
+                        (cons 0 (conj-assign-values (rest conj))))))
 
-(defun hornp (conj) 
-        (cond 
-        ((not (null conj)) 
-                (and (check-disj (first conj)) (hornp (rest conj))))
-        T))
+(defun horn (conjs)
+        (cond   ((not (null conjs))
+                        (cons (> 2 (suml (conj-assign-values (rest (first conjs))))) (horn (rest conjs))))
+                ((null conjs) 
+                        nil)))
 
-(print (hornp `(and (or p))))
+(defun hornp (dnf) 
+        (horn (rest dnf)))
+
+
+
+
+(print (hornp `(and (or (p X) (p Y) (not y)) (or (p X) (not k) (not y)))))
+(print (hornp `(and (or (not y)) (or (p X) (not k) (not y)))))
