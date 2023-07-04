d4236699328ace57b5d4e3b92473dfe656c79008
andreagosti9 <andrea.agosti9@gmail.com>
2023-07-04 09:17:05 +0200

    Create nopred.lisp

---
+++ lisp/nopred.lisp
@@ -0,0 +1,12 @@
+(defun nopred (p l)
+        (cond   ((null l) nil)
+                ((atom l) 
+                        (cond   ((null (funcall p l))
+                                        l)))
+                ((listp l)
+                        (cons (nopred p (first l)) (nopred p (rest l))))))
+
+(defun p (x) 
+        (or (equal x 2) (equal x 5) (equal x 8)))
+
+(print (nopred `p (list 2 6 5 (list 2 4 5) 9)))
