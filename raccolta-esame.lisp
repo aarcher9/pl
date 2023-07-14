f1e538f0656298f379911bd930de568c144bbc56
andreagosti9 <andrea.agosti9@gmail.com>
2023-07-14 15:01:12 +0200

    some more code

--- raccolta-esame.lisp
+++ raccolta-esame.lisp
@@ -1,5 +1,6 @@
 ;; Probabile, anzi quasi matematico che ci siano codici giá presenti nella repo, sono stati messi qui per comodità di accesso.
 
+
 ;; ===== Conversione in decimale da binario ===== ;;
 (defun 2to10 (b c)
         (cond   ((null b) 
@@ -10,9 +11,11 @@
 (defun bcd (b) 
         (2to10 b (length b)))
 
-;; (print (bcd `(1 0 0 0)))
-;; (print (bcd `(1 0 1 0 1 0)))
-;; (print (bcd `(0 0 0 1 0 0 0)))
+(defun t_10 () 
+        (print (bcd `(1 0 0 0)))
+        (print (bcd `(1 0 1 0 1 0)))
+        (print (bcd `(0 0 0 1 0 0 0))))
+
 
 ;; ===== repeated ===== ;;
 (defun square (x) (* x x))
@@ -22,8 +25,10 @@
                 ((= n 1) f)
                 (t (lambda (x) (funcall f (funcall (repeated f (1- n)) x))))))
 
-;; (print (funcall (repeated `square 2) 3))
-;; (print (funcall (repeated `square 3) 2))
+(defun t_20 ()
+        (print (funcall (repeated `square 2) 3))
+        (print (funcall (repeated `square 3) 2)))
+
 
 ;; ===== JSON key-value ===== ;;
 (defparameter e `(name "Ugo" surname "Mascetti"))
@@ -40,6 +45,30 @@
         (cond   ((or (null keys) (null values)) nil)
                 (t (cons (first keys) (cons (first values) (make-object (rest keys) (rest values)))) )))
 
-;; (print (get-value `name e))
-;; (print (add-key-value `addr "Via col vento" e))
-;; (print (make-object `(numero capelli) `(23 "biondi")))
+(defun t_30 ()
+        (print (get-value `name e))
+        (print (add-key-value `addr "Via col vento" e))
+        (print (make-object `(numero capelli) `(23 "biondi"))))
+
+
+;; ===== Conversione let in lambda ===== ;;
+(defun f (x y) 
+        (let    ((a (+ x (* x y))) (b (* 4 y)))
+                (lambda (y x) (+ (* x a) (* y (square b)) (* a b))) ))
+
+;; La trasformazione prevede soltanto di sostituire le variabili dichiarate nella let e di passarle come parametri alla lambda più interna in modo da salvarne il valore ma mascherarle dall'esterno.
+(defun g (x y)
+        (lambda (y x)
+                (funcall 
+                        (lambda (a b) (+ (* x a) (* y (square b)) (* a b)))
+                        (+ x (* x y))
+                        (* 4 y))))
+
+(defun t_40 () 
+        (print (funcall (f 2 2) 2 2)))
+
+(defun t_41 () 
+        (print (funcall (g 2 2) 2 2)))
+
+(t_40)
+(t_41)
--- raccolta-esame.pl
+++ raccolta-esame.pl
@@ -1,1 +1,64 @@
 % Probabile, anzi quasi matematico che ci siano codici giá presenti nella repo, sono stati messi qui per comodità di accesso.
+
+
+%% ===== Deepest Node ===== %%
+find(Node, Node, CD, CD).
+find(node(_, _, L, R), Node, D, ND) :- 
+        CD is D + 1, 
+        (find(L, Node, CD, ND) ; find(R, Node, CD, ND)).
+
+deepest_node(Node, DN, D) :- 
+        find(Node, DN, 0, D).
+
+t_10(DN, D) :- 
+        deepest_node(node(42, the_answer, nil, nil), DN, D).
+
+
+%% ===== Assoc If ===== %%
+is_three(3).
+
+% Scrivendolo in questo modo appena lo trova ritorna e non cerca oltre.
+assoc_if(_, [], []).
+assoc_if(Pred, [[K, V] | _], [K, V]) :- 
+        call(Pred, K).
+
+assoc_if(Pred, [_ | Tail], Res) :- 
+        assoc_if(Pred, Tail, Res).
+
+% Singolo
+t_20(R) :- 
+        assoc_if(is_three, [[2, two], [3, three], [3, three_again]], R).
+
+% Multiplo (per come implementato il predicato mi tocca usare bagof)
+t_21(R, Bag) :- 
+        bagof(R, assoc_if(is_three, [[2, two], [3, three], [3, three_again]], R), Bag).
+
+
+%% ===== Suffisso ===== %%
+% S è suffisso di L
+suffisso(S, S).
+suffisso(S, [_ | Tail]) :-
+        suffisso(S, Tail).
+
+t_30(S) :- 
+        suffisso(S, [a, s, foo(bar), 42]).
+
+t_31 :- 
+        suffisso([foo(bar)], [a, s, foo(bar), 42]).
+% > false.
+
+t_31 :- 
+        suffisso([foo(bar), 42], [a, s, foo(bar), 42]).
+% > true.
+
+
+%% ===== Double Item ===== %%
+double_item([], []).
+double_item([I | Tail], [I, I | Res]) :-
+        double_item(Tail, Res).
+
+t_40(Ds) :-
+        double_item([a, b, c], Ds).
+
+t_41 :-
+        double_item([a, b, b, c], [a, a, b, b, b, b, c, c]).
