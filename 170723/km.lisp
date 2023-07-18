74877da8f11e508abae77338abef37da2fd196dc
aarcher9 <andrea.agosti9@gmail.com>
2023-07-18 20:01:41 +0200

    added test for vector function

--- 170723/km.lisp
+++ 170723/km.lisp
@@ -8,6 +8,7 @@
 
 
 ;; --- Operazioni fra vettori
+;; Ci si rifereisce indistintamente ai vettori come punti e viceversa dal momento che sono strutture equivalenti in questo caso.
 
 ;; *
 (defun vplus (x y) 
@@ -16,22 +17,36 @@
 
 ;; *
 (defun vminus (x y) 
-        (print "differenza fra vettori"))
+        (vplus x (scalarprod -1 y)))
 
 ;; *
-(defun scalarprod (L y) 
-        (print "prodotto per uno scalare"))
+(defun scalarprod (L x) 
+        (if     (not (null x))
+                (cons (* L (first x)) (scalarprod L (rest x)))
+                nil))
 
 ;; *
+;; Note:
+;; - i vettori egualmente lunghi
 (defun innerprod (x y) 
-        (print "prodotto interno"))
+        (if     (not (null x))
+                (+ (* (first x) (first y)) (innerprod (rest x) (rest y)))
+                0))
 
 ;; *
 (defun norm (x) 
-        (print "prodotto interno"))
+        (expt (innerprod x x) (/ 2)))
+
+;;
+(defun distance (x y) 
+        (norm (vminus x y)))
 
 
 ;; --- Algoritmo k-means
+
+;; Per inizializzare i k centroidi iniziali devo avere a disposizione la dimensione dei punti
+(defun initialize(k n) 
+        ())
 
 ;; *
 (defun kmeans (observations k) 
@@ -42,18 +57,14 @@
         (print "centroid"))
 
 
-;; --- Supporto
 
+;; --- Parametri per testing 
+;; I test sono effettuati in un file a parte
 
-
-;; --- Test
 (defparameter Observations 
         `(      (3.0 7.0) (0.5 1.0) (0.8 0.5) (1.0 8.0) 
                 (0.9 1.2) (6.0 4.0) (7.0 5.5) (4.0 9.0) 
                 (9.0 4.0)))
-
-(print (vplus `(1 2) `(7 8)))
-(print `ciaooo)
 
 
 
---
+++ 170723/test.lisp
@@ -0,0 +1,22 @@
+(load "km.lisp")
+
+;; Prestare attenzione che 8 non equivale a 8.0 (come anche in altri linguaggi)
+;; Esiste la funzione (coerce ...) per le conversioni
+
+(assert (equal (vplus `(1 2) `(7 8)) `(8 10)))
+(assert (equal (vplus `(-1) `(4)) `(3)))
+(assert (equal (vplus `(0) `(0)) `(0)))
+
+(assert (equal (vminus `(1 2) `(7 8)) `(-6 -6)))
+(assert (equal (vminus `(-1) `(4)) `(-5)))
+(assert (equal (vminus `(0) `(0)) `(0)))
+
+(assert (equal (norm `(3 4)) 5.0))
+(assert (equal (norm `(0)) 0.0))
+
+(assert (equal (scalarprod 10 `(1 2 3)) `(10 20 30)))
+(assert (equal (scalarprod 10 `(0)) `(0)))
+
+(assert (equal (distance `(-2 -3) `(-1 -2)) (expt 2 (/ 2))))
+
+
